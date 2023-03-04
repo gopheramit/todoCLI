99ba550a7808552cab689daca9fa2a81e1fd7124
gopheramit <amitwpu@gmail.com>
2023-03-04 18:48:23 +0530

    completing v1 of tool

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	todo "github.com/gopheramit/todoCLI"
 )
 
-const todoFileName = ".todo.json"
+var todoFileName = ".todo.json"
 
 func main() {
 	flag.Usage = func() {
@@ -18,11 +21,14 @@ func main() {
 		flag.PrintDefaults()
 
 	}
-	task := flag.String("task", "", "task to be included in to do list")
+	add := flag.Bool("add", false, "task to be included in to do list")
 	list := flag.Bool("list", false, "list all the tasks")
 	complete := flag.Int("complete", 0, "item to be completed")
 	flag.Parse()
 
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
 	l := &todo.List{}
 	if err := l.Get(todoFileName); err != nil {
 
@@ -32,11 +38,12 @@ func main() {
 
 	switch {
 	case *list:
-		for _, item := range *l {
-			if !item.Done {
-				fmt.Println(item.Task)
-			}
-		}
+		// for _, item := range *l {
+		// 	if !item.Done {
+		// 		fmt.Println(item.Task)
+		// 	}
+		// }
+		fmt.Print(l)
 
 	case *complete > 0:
 		if err := l.Complete(*complete); err != nil {
@@ -48,8 +55,12 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		l.Add(t)
 
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -62,4 +73,20 @@ func main() {
 
 	}
 
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+	return s.Text(), nil
 }
